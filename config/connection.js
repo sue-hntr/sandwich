20b1e5dd7ce5299699418d3bb4385546054072c5
Suzanne Hunter <sue_huntr@yahoo.com>
2019-02-27 17:55:49 -0500

    Heroku deploy page 5)

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,17 @@ var mysql = require("mysql");
 
 require("dotenv").config();
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {  
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: process.env.DB_PASS,
   database: "sandwiches_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
