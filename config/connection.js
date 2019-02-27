e3c8661205c5b5d7e8e0b3561a30d083ee4406b3
Suzanne Hunter <sue_huntr@yahoo.com>
2019-02-27 17:30:46 -0500

    added dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,13 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+require("dotenv").config();
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "kyloren1",
+  password: process.env.DB_PASS,
   database: "sandwiches_db"
 });
 
